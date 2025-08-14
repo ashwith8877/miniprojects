ae1e74144e563a47875a0bf4e4c1217b9c1437e4
ashwith8877 <ashwithgoudchinnamraju@gmail.com>
2025-08-14 11:30:58 +0530

    Add files via upload
    My Portfolio

---
+++ Portfolio/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text',{
+    strings: ['Frontend','Backend','frameworks','and Data visualization tools'],
+    typeSpeed: 100,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+
+});
